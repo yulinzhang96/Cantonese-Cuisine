3f20e9abb8f819406880c475ad0d141c861d3516
Yulin Zhang <yuliz12@uci.edu>
2020-06-21 18:19:30 -0700

    Updates

---
+++ precache-manifest.f7e28a828549aea4406dd193a5ca0a83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41516d42ea21cc56b639d7ddaf24d24",
+    "url": "/Cantonese-Cuisine/index.html"
+  },
+  {
+    "revision": "f8f7b35fc7f5a7205012",
+    "url": "/Cantonese-Cuisine/static/css/main.1a45568c.chunk.css"
+  },
+  {
+    "revision": "7130ac136ea7e75d133c",
+    "url": "/Cantonese-Cuisine/static/js/2.c07ec10f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cantonese-Cuisine/static/js/2.c07ec10f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8f7b35fc7f5a7205012",
+    "url": "/Cantonese-Cuisine/static/js/main.0fb93691.chunk.js"
+  },
+  {
+    "revision": "dad65d46aafd9661a0f0",
+    "url": "/Cantonese-Cuisine/static/js/runtime-main.55e25006.js"
+  }
+]);
